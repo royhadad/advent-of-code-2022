5de6b835c7a5ea615e9fd415657a429b5f60d312
Roy Hadad <roy@datree.io>
2022-12-01 17:09:03 +0200

    refactor: add some more infra

---
+++ days/day3/problem.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./days/day3/input.txt", "utf-8");
+
+console.log("started");
+
+const res = 1;
+
+console.log(res);
+
+console.log("done");
---
+++ days/day5/problem.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./days/day5/input.txt", "utf-8");
+
+console.log("started");
+
+const res = 1;
+
+console.log(res);
+
+console.log("done");
