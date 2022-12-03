f84099920d1e633c83cabbf9fdbb77303a41c3e0
Roy Hadad <roy@datree.io>
2022-12-03 11:44:39 +0200

    feat: day 2 solved

---
+++ days/day2/problamPart2.ts
@@ -0,0 +1,120 @@
+import fs from "fs";
+
+type GameAction = "ROCK" | "PAPER" | "SCISSORS";
+type GameResult = "WIN" | "LOSE" | "DRAW";
+
+type GameWithDesiredResult = {
+  elf: GameAction;
+  desiredResult: GameResult;
+};
+
+type Game = GameWithDesiredResult & {
+  human: GameAction;
+};
+
+const convertGameActionCodeToGameActionType = (code: string): GameAction => {
+  if (code === "A") {
+    return "ROCK";
+  } else if (code === "B") {
+    return "PAPER";
+  } else if (code === "C") {
+    return "SCISSORS";
+  } else {
+    throw new Error("Invalid Game action code");
+  }
+};
+
+function convertGameDesiredResultCodeToType(
+  getDesiredResultCode: string
+): GameResult {
+  if (getDesiredResultCode === "X") {
+    return "LOSE";
+  } else if (getDesiredResultCode === "Y") {
+    return "DRAW";
+  } else if (getDesiredResultCode === "Z") {
+    return "WIN";
+  }
+  throw new Error("Invalid Game desiredResult code");
+}
+
+function getCorrectActionForHumanToGetDesiredResult(
+  gameWithDesiredResult: GameWithDesiredResult
+): GameAction {
+  if (
+    getGameResultForHuman({ ...gameWithDesiredResult, human: "ROCK" }) ===
+    gameWithDesiredResult.desiredResult
+  ) {
+    return "ROCK";
+  } else if (
+    getGameResultForHuman({ ...gameWithDesiredResult, human: "PAPER" }) ===
+    gameWithDesiredResult.desiredResult
+  ) {
+    return "PAPER";
+  } else if (
+    getGameResultForHuman({ ...gameWithDesiredResult, human: "SCISSORS" }) ===
+    gameWithDesiredResult.desiredResult
+  ) {
+    return "SCISSORS";
+  } else {
+    throw new Error("No correct action found");
+  }
+}
+
+function getGameResultForHuman(game: Game): GameResult {
+  if (game.human === "ROCK" && game.elf === "SCISSORS") {
+    return "WIN";
+  } else if (game.human === "PAPER" && game.elf === "ROCK") {
+    return "WIN";
+  } else if (game.human === "SCISSORS" && game.elf === "PAPER") {
+    return "WIN";
+  } else if (game.human === game.elf) {
+    return "DRAW";
+  } else {
+    return "LOSE";
+  }
+}
+
+const input = fs.readFileSync("./days/day2/input.txt", "utf-8").trim();
+
+console.log("started");
+
+const games = input
+  .split("\n")
+  .map((s) => {
+    return s.split(" ");
+  })
+  .map<GameWithDesiredResult>(([elfCode, desiredResultCode]) => {
+    return {
+      elf: convertGameActionCodeToGameActionType(elfCode),
+      desiredResult: convertGameDesiredResultCodeToType(desiredResultCode),
+    };
+  })
+  .map<Game>((gameWithDesiredResult) => {
+    return {
+      ...gameWithDesiredResult,
+      human: getCorrectActionForHumanToGetDesiredResult(gameWithDesiredResult),
+    };
+  });
+
+let totalScore = 0;
+games.forEach((game) => {
+  if (game.human === "ROCK") {
+    totalScore += 1;
+  } else if (game.human === "PAPER") {
+    totalScore += 2;
+  } else if (game.human === "SCISSORS") {
+    totalScore += 3;
+  }
+
+  if (getGameResultForHuman(game) === "WIN") {
+    totalScore += 6;
+  } else if (getGameResultForHuman(game) === "DRAW") {
+    totalScore += 3;
+  }
+});
+
+const res = totalScore;
+
+console.log(res);
+
+console.log("done");
--- days/day2/problem.ts
+++ days/day2/problem.ts
@@ -1,10 +1,74 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./days/day2/input.txt", "utf-8");
+type GameAction = "ROCK" | "PAPER" | "SCISSORS";
+
+type Game = {
+  elf: GameAction;
+  human: GameAction;
+};
+
+type GameResult = "WIN" | "LOSE" | "DRAW";
+
+const gameActionCodeToGameAction = (code: string): GameAction => {
+  if (code === "A" || code === "X") {
+    return "ROCK";
+  } else if (code === "B" || code === "Y") {
+    return "PAPER";
+  } else if (code === "C" || code === "Z") {
+    return "SCISSORS";
+  } else {
+    throw new Error("Invalid Game action code");
+  }
+};
+
+function getGameResultForHuman(game: Game): GameResult {
+  if (game.human === "ROCK" && game.elf === "SCISSORS") {
+    return "WIN";
+  } else if (game.human === "PAPER" && game.elf === "ROCK") {
+    return "WIN";
+  } else if (game.human === "SCISSORS" && game.elf === "PAPER") {
+    return "WIN";
+  } else if (game.human === game.elf) {
+    return "DRAW";
+  } else {
+    return "LOSE";
+  }
+}
+
+const input = fs.readFileSync("./days/day2/input.txt", "utf-8").trim();
 
 console.log("started");
 
-const res = 1;
+const games = input
+  .split("\n")
+  .map((s) => {
+    return s.split(" ");
+  })
+  .map<Game>(([elfCode, meCode]) => {
+    return {
+      elf: gameActionCodeToGameAction(elfCode),
+      human: gameActionCodeToGameAction(meCode),
+    };
+  });
+
+let totalScore = 0;
+games.forEach((game) => {
+  if (game.human === "ROCK") {
+    totalScore += 1;
+  } else if (game.human === "PAPER") {
+    totalScore += 2;
+  } else if (game.human === "SCISSORS") {
+    totalScore += 3;
+  }
+
+  if (getGameResultForHuman(game) === "WIN") {
+    totalScore += 6;
+  } else if (getGameResultForHuman(game) === "DRAW") {
+    totalScore += 3;
+  }
+});
+
+const res = totalScore;
 
 console.log(res);
 
