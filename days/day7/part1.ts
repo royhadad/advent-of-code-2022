ef80ba6a00195b7d8fac7ce20b28fea574442892
Roy Hadad <roy@datree.io>
2022-12-07 12:26:04 +0200

    feat: part 1 solved

--- days/day7/part1.ts
+++ days/day7/part1.ts
@@ -32,35 +32,135 @@ type LsOutputFile = {
   size: number;
 };
 
-const inputRows = input.split("\n");
+function parseInput(input: string): InputRow[] {
+  const inputRows = input.split("\n");
 
-const parsedInputRows: InputRow[] = inputRows.map<InputRow>((row) => {
-  if (row.startsWith("$ cd ")) {
-    return {
-      type: InputRowType.CD,
-      path: row.slice(5),
-    };
-  } else if (row.startsWith("$ ls")) {
-    return {
-      type: InputRowType.LS,
-    };
-  } else if (row.startsWith("dir ")) {
-    return {
-      type: InputRowType.LS_OUTPUT_DIR,
-      name: row.slice(4),
-    };
-  } else if (row.match(/^\d+[ ]\w+(\.\w+)?$/)) {
-    return {
-      type: InputRowType.LS_OUTPUT_FILE,
-      name: row.slice(row.lastIndexOf(" ") + 1),
-      size: parseInt(row.slice(0, row.lastIndexOf(" "))),
-    };
-  } else {
-    throw new Error("Unknown input row: " + row);
+  const parsedInputRows: InputRow[] = inputRows.map<InputRow>((row) => {
+    if (row.startsWith("$ cd ")) {
+      return {
+        type: InputRowType.CD,
+        path: row.slice(5),
+      };
+    } else if (row.startsWith("$ ls")) {
+      return {
+        type: InputRowType.LS,
+      };
+    } else if (row.startsWith("dir ")) {
+      return {
+        type: InputRowType.LS_OUTPUT_DIR,
+        name: row.slice(4),
+      };
+    } else if (row.match(/^\d+[ ]\w+(\.\w+)?$/)) {
+      return {
+        type: InputRowType.LS_OUTPUT_FILE,
+        name: row.slice(row.lastIndexOf(" ") + 1),
+        size: parseInt(row.slice(0, row.lastIndexOf(" "))),
+      };
+    } else {
+      throw new Error("Unknown input row: " + row);
+    }
+  });
+  return parsedInputRows;
+}
+
+type Directory = {
+  name: string;
+  size: number | null;
+  directories: Directory[];
+  files: File[];
+  parentDirectory: Directory | null;
+};
+
+type File = {
+  name: string;
+  size: number;
+};
+
+function convertParsedInputToFileSystem(parsedInputRows: InputRow[]): Directory {
+  const fileSystem: Directory = {
+    name: "/",
+    size: null,
+    files: [],
+    directories: [],
+    parentDirectory: null,
+  };
+
+  let isCurrentlyLsOutput: boolean = false;
+  let currentDirectory: Directory = fileSystem;
+  for (let i = 0; i < parsedInputRows.length; i++) {
+    const row = parsedInputRows[i];
+
+    isCurrentlyLsOutput = row.type === InputRowType.LS;
+
+    if (row.type === InputRowType.CD) {
+      if (row.path === "/") {
+        currentDirectory = fileSystem;
+      } else if (row.path === ".." && currentDirectory.parentDirectory) {
+        currentDirectory = currentDirectory.parentDirectory;
+      } else {
+        const directory = currentDirectory.directories.find((dir) => dir.name === row.path);
+        if (directory) {
+          currentDirectory = directory;
+        } else {
+          throw new Error("Unknown directory: " + row.path);
+        }
+      }
+    } else if (row.type === InputRowType.LS_OUTPUT_DIR) {
+      currentDirectory.directories.push({
+        name: row.name,
+        files: [],
+        directories: [],
+        size: null,
+        parentDirectory: currentDirectory,
+      });
+    } else if (row.type === InputRowType.LS_OUTPUT_FILE) {
+      currentDirectory.files.push({
+        name: row.name,
+        size: row.size,
+      });
+    }
   }
-});
 
-const res = parsedInputRows;
+  return fileSystem;
+}
+
+type DirectoryWithSize = Directory & { size: number; directories: DirectoryWithSize[] };
+
+function addDirectorySizesToEntireFileSystem(directory: Directory): DirectoryWithSize {
+  const childrenFileSizes = directory.files.reduce((acc, file) => acc + file.size, 0);
+  const childrenDirectorySizes = directory.directories.reduce((acc, dir) => {
+    if (dir.size === null) {
+      addDirectorySizesToEntireFileSystem(dir);
+    }
+    if (dir.size === null) {
+      throw new Error("dir.size is still null for some reason");
+    }
+    return acc + dir.size;
+  }, 0);
+
+  directory.size = childrenFileSizes + childrenDirectorySizes;
+  return directory as DirectoryWithSize;
+}
+
+function getAllDirectoriesWithUpToSize(fileSystem: DirectoryWithSize, size: number): DirectoryWithSize[] {
+  const directoriesWithSizeUpTo100000: DirectoryWithSize[] = [];
+  function findDirectoriesWithMaxSize(directory: DirectoryWithSize): void {
+    if (directory.size <= size) {
+      directoriesWithSizeUpTo100000.push(directory);
+    }
+    directory.directories.forEach(findDirectoriesWithMaxSize);
+  }
+  findDirectoriesWithMaxSize(fileSystem);
+  return directoriesWithSizeUpTo100000;
+}
+
+const parsedInputRows = parseInput(input);
+const fileSystem = convertParsedInputToFileSystem(parsedInputRows);
+const fileSystemWithDirectorySizes = addDirectorySizesToEntireFileSystem(fileSystem);
+const directoriesWithSizeUpTo100000 = getAllDirectoriesWithUpToSize(fileSystemWithDirectorySizes, 100000);
+const sumOfAllDirectoriesWithSizeUpTo100000 = directoriesWithSizeUpTo100000.reduce((acc, dir) => acc + dir.size, 0);
+
+const res = sumOfAllDirectoriesWithSizeUpTo100000;
 
 console.log(res);
 
