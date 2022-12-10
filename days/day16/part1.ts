55fc97b9f59dabbe81a2231d42dcee57376a2418
Roy Hadad <roy@datree.io>
2022-12-10 16:56:29 +0200

    refactor: add infra for days 11-18

---
+++ days/day16/part1.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
+
+console.log("started");
+
+const res = 1;
+
+console.log(res);
+
+console.log("done");
