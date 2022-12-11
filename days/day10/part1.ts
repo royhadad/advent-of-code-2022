cc7c6aab645846d295ea29d943ec91be56624cdb
Roy Hadad <roy@datree.io>
2022-12-11 23:36:21 +0200

    feat: day 10 part 1 solved

--- days/day10/part1.ts
+++ days/day10/part1.ts
@@ -1,11 +1,54 @@
 import fs from "fs";
 
-const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
+export function getValueOfRegisterXAtEveryStartOfCycle(): number[] {
+  const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
+  const inputRows = input.split("\n");
 
-console.log("started");
+  let registerX = 1;
+  let currentCycle = 1;
+  const valueOfRegisterXAtEveryStartOfCycle: number[] = [1];
 
-const res = 1;
+  inputRows.forEach((row) => {
+    if (row === "noop") {
+      valueOfRegisterXAtEveryStartOfCycle.push(registerX);
+      currentCycle++;
+    } else if (row.startsWith("addx")) {
+      valueOfRegisterXAtEveryStartOfCycle.push(registerX);
+      currentCycle++;
+      valueOfRegisterXAtEveryStartOfCycle.push(registerX);
+      currentCycle++;
+      const valueToAdd = Number(row.split(" ")[1]);
+      registerX += valueToAdd;
+    } else {
+      throw new Error("Unknown instruction: " + row);
+    }
+  });
 
-console.log(res);
+  return valueOfRegisterXAtEveryStartOfCycle;
+}
 
-console.log("done");
+function main(): void {
+  console.log("started");
+
+  const valueOfRegisterXAtEveryStartOfCycle = getValueOfRegisterXAtEveryStartOfCycle();
+
+  const signalStrengthAtEveryStartOfCycle = valueOfRegisterXAtEveryStartOfCycle.map(
+    (registerX, cycleNumber) => registerX * cycleNumber
+  );
+
+  const sumOfSignalStrengthsAtSpecificIndices =
+    signalStrengthAtEveryStartOfCycle[20] +
+    signalStrengthAtEveryStartOfCycle[60] +
+    signalStrengthAtEveryStartOfCycle[100] +
+    signalStrengthAtEveryStartOfCycle[140] +
+    signalStrengthAtEveryStartOfCycle[180] +
+    signalStrengthAtEveryStartOfCycle[220];
+
+  const res = sumOfSignalStrengthsAtSpecificIndices;
+
+  console.log(res);
+
+  console.log("done");
+}
+
+main();
