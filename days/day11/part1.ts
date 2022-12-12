a8e87ae848e9871b75ba8496478fe8bd465e4fcd
Roy Hadad <roy@datree.io>
2022-12-12 02:39:23 +0200

    refactor: day 11 refactor

--- days/day11/part1.ts
+++ days/day11/part1.ts
@@ -2,7 +2,7 @@ export type Item = number;
 export type Monkey = {
   items: Item[];
   operation: (oldValue: Item) => Item;
-  test: (oldValue: Item) => boolean;
+  testDivisibilityBy: number;
   monkeyToThrowToOnTestSuccess: number;
   monkeyToThrowToOnTestFailure: number;
   numberOfItemsInspected: number;
@@ -13,7 +13,7 @@ export const monkeys: Monkey[] = [
   {
     items: [63, 57],
     operation: (oldValue) => oldValue * 11,
-    test: (oldValue) => oldValue % 7 === 0,
+    testDivisibilityBy: 7,
     monkeyToThrowToOnTestSuccess: 6,
     monkeyToThrowToOnTestFailure: 2,
     numberOfItemsInspected: 0,
@@ -21,7 +21,7 @@ export const monkeys: Monkey[] = [
   {
     items: [82, 66, 87, 78, 77, 92, 83],
     operation: (oldValue) => oldValue + 1,
-    test: (oldValue) => oldValue % 11 === 0,
+    testDivisibilityBy: 11,
     monkeyToThrowToOnTestSuccess: 5,
     monkeyToThrowToOnTestFailure: 0,
     numberOfItemsInspected: 0,
@@ -29,7 +29,7 @@ export const monkeys: Monkey[] = [
   {
     items: [97, 53, 53, 85, 58, 54],
     operation: (oldValue) => oldValue * 7,
-    test: (oldValue) => oldValue % 13 === 0,
+    testDivisibilityBy: 13,
     monkeyToThrowToOnTestSuccess: 4,
     monkeyToThrowToOnTestFailure: 3,
     numberOfItemsInspected: 0,
@@ -37,7 +37,7 @@ export const monkeys: Monkey[] = [
   {
     items: [50],
     operation: (oldValue) => oldValue + 3,
-    test: (oldValue) => oldValue % 3 === 0,
+    testDivisibilityBy: 3,
     monkeyToThrowToOnTestSuccess: 1,
     monkeyToThrowToOnTestFailure: 7,
     numberOfItemsInspected: 0,
@@ -45,7 +45,7 @@ export const monkeys: Monkey[] = [
   {
     items: [64, 69, 52, 65, 73],
     operation: (oldValue) => oldValue + 6,
-    test: (oldValue) => oldValue % 17 === 0,
+    testDivisibilityBy: 17,
     monkeyToThrowToOnTestSuccess: 3,
     monkeyToThrowToOnTestFailure: 7,
     numberOfItemsInspected: 0,
@@ -53,7 +53,7 @@ export const monkeys: Monkey[] = [
   {
     items: [57, 91, 65],
     operation: (oldValue) => oldValue + 5,
-    test: (oldValue) => oldValue % 2 === 0,
+    testDivisibilityBy: 2,
     monkeyToThrowToOnTestSuccess: 0,
     monkeyToThrowToOnTestFailure: 6,
     numberOfItemsInspected: 0,
@@ -61,7 +61,7 @@ export const monkeys: Monkey[] = [
   {
     items: [67, 91, 84, 78, 60, 69, 99, 83],
     operation: (oldValue) => oldValue * oldValue,
-    test: (oldValue) => oldValue % 5 === 0,
+    testDivisibilityBy: 5,
     monkeyToThrowToOnTestSuccess: 2,
     monkeyToThrowToOnTestFailure: 4,
     numberOfItemsInspected: 0,
@@ -69,12 +69,22 @@ export const monkeys: Monkey[] = [
   {
     items: [58, 78, 69, 65],
     operation: (oldValue) => oldValue + 7,
-    test: (oldValue) => oldValue % 19 === 0,
+    testDivisibilityBy: 19,
     monkeyToThrowToOnTestSuccess: 5,
     monkeyToThrowToOnTestFailure: 1,
     numberOfItemsInspected: 0,
   },
 ];
+
+export function calculateLevelOfMonkeyBusiness(monkeys: Monkey[]): number {
+  const numberOfItemsEachMonkeyInspectedSortedDesc = monkeys
+    .map((monkey) => monkey.numberOfItemsInspected)
+    .sort((a, b) => b - a);
+
+  const levelOfMonkeyBusiness =
+    numberOfItemsEachMonkeyInspectedSortedDesc[0] * numberOfItemsEachMonkeyInspectedSortedDesc[1];
+  return levelOfMonkeyBusiness;
+}
 
 function main(): void {
   console.log("started");
@@ -85,7 +95,7 @@ function main(): void {
       monkey.items.forEach((item, index) => {
         const itemWithNewWorryLevel = Math.floor(monkey.operation(item) / 3);
         monkey.numberOfItemsInspected++;
-        if (monkey.test(itemWithNewWorryLevel)) {
+        if (itemWithNewWorryLevel % monkey.testDivisibilityBy === 0) {
           monkeys[monkey.monkeyToThrowToOnTestSuccess].items.push(itemWithNewWorryLevel);
         } else {
           monkeys[monkey.monkeyToThrowToOnTestFailure].items.push(itemWithNewWorryLevel);
@@ -95,13 +105,7 @@ function main(): void {
     });
   }
 
-  const numberOfItemsEachMonkeyInspectedSortedDesc = monkeys
-    .map((monkey) => monkey.numberOfItemsInspected)
-    .sort((a, b) => b - a);
-
-  const levelOfMonkeyBusiness =
-    numberOfItemsEachMonkeyInspectedSortedDesc[0] * numberOfItemsEachMonkeyInspectedSortedDesc[1];
-  const res = levelOfMonkeyBusiness;
+  const res = calculateLevelOfMonkeyBusiness(monkeys);
 
   console.log(res);
 
--- days/day11/part2.ts
+++ days/day11/part2.ts
@@ -1,121 +1,41 @@
 import lcm from "compute-lcm";
+import { calculateLevelOfMonkeyBusiness, Monkey, monkeys } from "./part1";
 
-export type Item = number;
-export type Monkey = {
-  items: Item[];
-  operation: (oldValue: Item) => Item;
-  testDivisibilityBy: number;
-  monkeyToThrowToOnTestSuccess: number;
-  monkeyToThrowToOnTestFailure: number;
-  numberOfItemsInspected: number;
-};
-
-// the input, parsed by hand
-export const monkeys: Monkey[] = [
-  {
-    items: [63, 57],
-    operation: (oldValue) => oldValue * 11,
-    testDivisibilityBy: 7,
-    monkeyToThrowToOnTestSuccess: 6,
-    monkeyToThrowToOnTestFailure: 2,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [82, 66, 87, 78, 77, 92, 83],
-    operation: (oldValue) => oldValue + 1,
-    testDivisibilityBy: 11,
-    monkeyToThrowToOnTestSuccess: 5,
-    monkeyToThrowToOnTestFailure: 0,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [97, 53, 53, 85, 58, 54],
-    operation: (oldValue) => oldValue * 7,
-    testDivisibilityBy: 13,
-    monkeyToThrowToOnTestSuccess: 4,
-    monkeyToThrowToOnTestFailure: 3,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [50],
-    operation: (oldValue) => oldValue + 3,
-    testDivisibilityBy: 3,
-    monkeyToThrowToOnTestSuccess: 1,
-    monkeyToThrowToOnTestFailure: 7,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [64, 69, 52, 65, 73],
-    operation: (oldValue) => oldValue + 6,
-    testDivisibilityBy: 17,
-    monkeyToThrowToOnTestSuccess: 3,
-    monkeyToThrowToOnTestFailure: 7,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [57, 91, 65],
-    operation: (oldValue) => oldValue + 5,
-    testDivisibilityBy: 2,
-    monkeyToThrowToOnTestSuccess: 0,
-    monkeyToThrowToOnTestFailure: 6,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [67, 91, 84, 78, 60, 69, 99, 83],
-    operation: (oldValue) => oldValue * oldValue,
-    testDivisibilityBy: 5,
-    monkeyToThrowToOnTestSuccess: 2,
-    monkeyToThrowToOnTestFailure: 4,
-    numberOfItemsInspected: 0,
-  },
-  {
-    items: [58, 78, 69, 65],
-    operation: (oldValue) => oldValue + 7,
-    testDivisibilityBy: 19,
-    monkeyToThrowToOnTestSuccess: 5,
-    monkeyToThrowToOnTestFailure: 1,
-    numberOfItemsInspected: 0,
-  },
-];
-
-function main(): void {
-  console.log("started");
-
+function getLowestCommonMultipleOfAllDivisibilityNumbers(monkeys: Monkey[]): number {
   const lowestCommonMultipleOfAllDivisibilityNumbers = lcm(monkeys.map((monkey) => monkey.testDivisibilityBy));
-
   if (lowestCommonMultipleOfAllDivisibilityNumbers === null || isNaN(lowestCommonMultipleOfAllDivisibilityNumbers)) {
     throw new Error("Could not calculate LCM");
   } else {
     console.log("lowestCommonMultipleOfAllDivisibilityNumbers", lowestCommonMultipleOfAllDivisibilityNumbers);
   }
+  return lowestCommonMultipleOfAllDivisibilityNumbers;
+}
+
+function main(): void {
+  console.log("started");
+
+  const lowestCommonMultipleOfAllDivisibilityNumbers = getLowestCommonMultipleOfAllDivisibilityNumbers(monkeys);
 
   for (let roundNumber = 1; roundNumber <= 10000; roundNumber++) {
     console.log(`Starting round ${roundNumber}`);
-    if (roundNumber % 100 === 0) {
-      console.log("DIVISIBLE BY 100");
-    }
     monkeys.forEach((monkey) => {
       monkey.items.forEach((item, index) => {
-        const itemWithNewWorryLevel = monkey.operation(item);
-        const itemWithNewWorryLevelReduced = itemWithNewWorryLevel % lowestCommonMultipleOfAllDivisibilityNumbers;
+        // because only the modulo of the item is relevant, we can take only the modulo of the item by the LCM of all divisibility numbers
+        // this does not affect the result because of modular arithmetic
+        // "I have discovered a truly marvelous proof of this, which this margin is too narrow to contain." - Pierre de Fermat
+        const itemWithNewWorryLevel = monkey.operation(item) % lowestCommonMultipleOfAllDivisibilityNumbers;
         monkey.numberOfItemsInspected++;
-        if (itemWithNewWorryLevelReduced % monkey.testDivisibilityBy === 0) {
-          monkeys[monkey.monkeyToThrowToOnTestSuccess].items.push(itemWithNewWorryLevelReduced);
+        if (itemWithNewWorryLevel % monkey.testDivisibilityBy === 0) {
+          monkeys[monkey.monkeyToThrowToOnTestSuccess].items.push(itemWithNewWorryLevel);
         } else {
-          monkeys[monkey.monkeyToThrowToOnTestFailure].items.push(itemWithNewWorryLevelReduced);
+          monkeys[monkey.monkeyToThrowToOnTestFailure].items.push(itemWithNewWorryLevel);
         }
       });
       monkey.items = [];
     });
   }
 
-  const numberOfItemsEachMonkeyInspectedSortedDesc = monkeys
-    .map((monkey) => monkey.numberOfItemsInspected)
-    .sort((a, b) => b - a);
-
-  const levelOfMonkeyBusiness =
-    numberOfItemsEachMonkeyInspectedSortedDesc[0] * numberOfItemsEachMonkeyInspectedSortedDesc[1];
-  const res = levelOfMonkeyBusiness;
+  const res = calculateLevelOfMonkeyBusiness(monkeys);
 
   console.log(res);
 
