90d95136c66a825695870f985b9f7be7edd553e0
Roy Hadad <roy@datree.io>
2022-12-12 18:24:34 +0200

    feat: day 12 part 1 parse input

--- days/day12/part1.ts
+++ days/day12/part1.ts
@@ -1,10 +1,50 @@
 import fs from "fs";
 
-const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
-
 console.log("started");
 
-const res = 1;
+// a-z
+type Square = number; // 0-25
+type Map = Square[][];
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+function parseInputToMap(input: string): { map: Map; start: Position; end: Position } {
+  const start: Position = { x: -1, y: -1 };
+  const end: Position = { x: -1, y: -1 };
+
+  const map: Map = input.split("\n").map((line, yIndex) =>
+    line.split("").map((char, xIndex) => {
+      if (char === "S") {
+        start.x = xIndex;
+        start.y = yIndex;
+        return 0;
+      }
+      if (char === "E") {
+        end.x = xIndex;
+        end.y = yIndex;
+        return 25;
+      }
+      return char.charCodeAt(0) - 97;
+    })
+  );
+
+  if (start.x === -1 || start.y === -1 || end.x === -1 || end.y === -1) {
+    throw new Error("start or end not found");
+  }
+
+  return { map, start, end };
+}
+
+const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
+const { map, start, end } = parseInputToMap(input);
+
+// TODO find path with least steps
+// only one step up at a time at most
+
+const res = map;
 
 console.log(res);
 
