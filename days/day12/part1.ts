f4355aa2624864a4178e9313bf19651d128a1dc6
Roy Hadad <roy@datree.io>
2022-12-18 01:13:50 +0200

    feat: day 12 part 1 solved

--- days/day12/part1.ts
+++ days/day12/part1.ts
@@ -14,49 +14,27 @@ import {
 
 console.log("started");
 
-function isPositionAlreadyVisited(currentPosition: HistoryTreeNode, positionToCheck: Position): boolean {
-  if (arePositionsEqual(currentPosition.position, positionToCheck)) {
-    return true;
-  }
-  if (currentPosition.previousPosition === null) {
-    return false;
-  }
-  return isPositionAlreadyVisited(currentPosition.previousPosition, positionToCheck);
-}
-
 function getPossibleNextPositions(map: Map, start: HistoryTreeNode): Position[] {
   const possibleMoves: Position[] = [];
 
   const currentPosition = start.position;
   const positionAfterMoveLeft = { ...currentPosition, x: currentPosition.x - 1 };
-  if (
-    isMovePossible(currentPosition, positionAfterMoveLeft, map) &&
-    !isPositionAlreadyVisited(start, positionAfterMoveLeft)
-  ) {
+  if (isMovePossible(currentPosition, positionAfterMoveLeft, map)) {
     possibleMoves.push(positionAfterMoveLeft);
   }
 
   const positionAfterMoveRight = { ...currentPosition, x: currentPosition.x + 1 };
-  if (
-    isMovePossible(currentPosition, positionAfterMoveRight, map) &&
-    !isPositionAlreadyVisited(start, positionAfterMoveRight)
-  ) {
+  if (isMovePossible(currentPosition, positionAfterMoveRight, map)) {
     possibleMoves.push(positionAfterMoveRight);
   }
 
   const positionAfterMoveUp = { ...currentPosition, y: currentPosition.y - 1 };
-  if (
-    isMovePossible(currentPosition, positionAfterMoveUp, map) &&
-    !isPositionAlreadyVisited(start, positionAfterMoveUp)
-  ) {
+  if (isMovePossible(currentPosition, positionAfterMoveUp, map)) {
     possibleMoves.push(positionAfterMoveUp);
   }
 
   const positionAfterMoveDown = { ...currentPosition, y: currentPosition.y + 1 };
-  if (
-    isMovePossible(currentPosition, positionAfterMoveDown, map) &&
-    !isPositionAlreadyVisited(start, positionAfterMoveDown)
-  ) {
+  if (isMovePossible(currentPosition, positionAfterMoveDown, map)) {
     possibleMoves.push(positionAfterMoveDown);
   }
 
@@ -69,46 +47,114 @@ type HistoryTreeNode = {
   nextPositions: HistoryTreeNode[];
 };
 
-function createRoutesTree(map: Map, start: Position, end: Position): HistoryTreeNode {
+// function createRoutesTree(map: Map, start: Position, end: Position): HistoryTreeNode {
+//   const rootTreeNode: HistoryTreeNode = {
+//     position: start,
+//     previousPosition: null,
+//     nextPositions: [],
+//   };
+//   createRoutesTreeRecursive(map, end, rootTreeNode, rootTreeNode);
+//   return rootTreeNode;
+// }
+//
+// function createRoutesTreeRecursive(
+//   map: Map,
+//   end: Position,
+//   rootTreeNode: HistoryTreeNode,
+//   currentTreeNode: HistoryTreeNode
+// ): void {
+//   if (arePositionsEqual(currentTreeNode.position, end)) {
+//     return;
+//   }
+//   currentTreeNode.nextPositions = getPossibleNextPositions(map, currentTreeNode).map((position) => ({
+//     position,
+//     previousPosition: currentTreeNode,
+//     nextPositions: [],
+//   }));
+//   currentTreeNode.nextPositions.forEach((nextPosition) => {
+//     createRoutesTreeRecursive(map, end, rootTreeNode, nextPosition);
+//   });
+// }
+
+class VisitedPositionsMap {
+  private readonly map;
+  constructor() {
+    this.map = new Map<string, boolean>();
+  }
+  private positionToUniqueKey(position: Position): string {
+    return `${position.x},${position.y}`;
+  }
+
+  public isVisited(position: Position): boolean {
+    return this.map.get(this.positionToUniqueKey(position)) ?? false;
+  }
+  public markAsVisited(position: Position): void {
+    this.map.set(this.positionToUniqueKey(position), true);
+  }
+}
+
+function createRoutesTreeBFS(map: Map, start: Position, end: Position): HistoryTreeNode {
+  const positionsThatHaveAlreadyBeenVisited = new VisitedPositionsMap();
+
   const rootTreeNode: HistoryTreeNode = {
     position: start,
     previousPosition: null,
     nextPositions: [],
   };
-  createRoutesTreeRecursive(map, end, rootTreeNode, rootTreeNode);
-  return rootTreeNode;
+  return createRoutesTreeRecursiveBFS(map, positionsThatHaveAlreadyBeenVisited, end, [rootTreeNode]);
 }
 
-function createRoutesTreeRecursive(
+function createRoutesTreeRecursiveBFS(
   map: Map,
+  positionsThatHaveAlreadyBeenVisited: VisitedPositionsMap,
   end: Position,
-  rootTreeNode: HistoryTreeNode,
-  currentTreeNode: HistoryTreeNode
-): void {
-  if (arePositionsEqual(currentTreeNode.position, end)) {
-    return;
+  currentTreeNodes: HistoryTreeNode[]
+): HistoryTreeNode {
+  // if one of the current positions is the end positions, we found the shortest route, return it!
+  for (const currentTreeNode of currentTreeNodes) {
+    if (arePositionsEqual(currentTreeNode.position, end)) {
+      return currentTreeNode;
+    }
   }
-  currentTreeNode.nextPositions = getPossibleNextPositions(map, currentTreeNode).map((position) => ({
-    position,
-    previousPosition: currentTreeNode,
-    nextPositions: [],
-  }));
-  currentTreeNode.nextPositions.forEach((nextPosition) => {
-    createRoutesTreeRecursive(map, end, rootTreeNode, nextPosition);
+
+  // get the next nodes for each node and flatten it
+  const nextNodes = currentTreeNodes.flatMap<HistoryTreeNode>((currentNode) => {
+    return getPossibleNextPositions(map, currentNode).map<HistoryTreeNode>((position) => ({
+      position,
+      previousPosition: currentNode,
+      nextPositions: [],
+    }));
   });
+
+  // get rid of positions that have already been visited, remove duplicates as well
+  const nextNodesFiltered = nextNodes.filter((nextNode) => {
+    const shouldKeepPosition = !positionsThatHaveAlreadyBeenVisited.isVisited(nextNode.position);
+    if (shouldKeepPosition) {
+      positionsThatHaveAlreadyBeenVisited.markAsVisited(nextNode.position);
+    }
+    return shouldKeepPosition;
+  });
+
+  return createRoutesTreeRecursiveBFS(map, positionsThatHaveAlreadyBeenVisited, end, nextNodesFiltered);
 }
 
 function findShortestRoute(map: Map, start: Position, end: Position): Position[] {
-  const routesTree = createRoutesTree(map, start, end);
+  const routesTree = createRoutesTreeBFS(map, start, end);
 
-  console.log(routesTree);
+  const route: Position[] = [];
+  let current: HistoryTreeNode | null = routesTree;
+  while (current !== null) {
+    route.push(current.position);
+    current = current.previousPosition;
+  }
 
-  return [];
+  return route.reverse();
 }
 
 const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
 const { map, start, end } = parseInputToMap(input);
-const shortestRouteLength = findShortestRoute(map, start, end).length;
+const shortestRoute = findShortestRoute(map, start, end);
+const shortestRouteLength = shortestRoute.length - 1;
 
 const res = shortestRouteLength;
 
