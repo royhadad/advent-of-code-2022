c48134c71aee998343fef9053072dee83c43627a
Roy Hadad <roy@datree.io>
2022-12-18 00:16:55 +0200

    refactor: move some stuff to local util

--- days/day12/part1.ts
+++ days/day12/part1.ts
@@ -1,90 +1,18 @@
 import fs from "fs";
+import {
+  Map,
+  Position,
+  Move,
+  Altitude,
+  parseInputToMap,
+  arePositionsEqual,
+  isMovePossible,
+  getAltitudeAtPosition,
+  isPositionInMap,
+  getTwoDimensionalDistanceBetweenTwoPositions,
+} from "./util";
 
 console.log("started");
-
-// a-z
-type Altitude = number; // 0-25
-type Map = Altitude[][];
-
-type Position = {
-  x: number;
-  y: number;
-};
-
-enum Move {
-  UP = "up",
-  DOWN = "down",
-  LEFT = "left",
-  RIGHT = "right",
-}
-
-function parseInputToMap(input: string): { map: Map; start: Position; end: Position } {
-  const start: Position = { x: -1, y: -1 };
-  const end: Position = { x: -1, y: -1 };
-
-  const map: Map = input.split("\n").map((line, yIndex) =>
-    line.split("").map((char, xIndex) => {
-      if (char === "S") {
-        start.x = xIndex;
-        start.y = yIndex;
-        return 0;
-      }
-      if (char === "E") {
-        end.x = xIndex;
-        end.y = yIndex;
-        return 25;
-      }
-      return char.charCodeAt(0) - 97;
-    })
-  );
-
-  if (start.x === -1 || start.y === -1 || end.x === -1 || end.y === -1) {
-    throw new Error("start or end not found");
-  }
-
-  return { map, start, end };
-}
-
-function getAltitudeAtPosition(position: Position, map: Map): Altitude {
-  const res = map[position.y]?.[position.x];
-  if (res === undefined) {
-    throw new Error("position out of bounds");
-  }
-  return res;
-}
-
-function isPositionInMap(position: Position, map: Map): boolean {
-  try {
-    const res = getAltitudeAtPosition(position, map);
-    if (res === undefined) {
-      return false;
-    }
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function getTwoDimensionalDistanceBetweenTwoPositions(position1: Position, position2: Position): number {
-  return Math.hypot(position2.x - position1.x, position2.y - position1.y);
-}
-
-function isMovePossible(start: Position, destinationPosition: Position, map: Map): boolean {
-  if (!isPositionInMap(destinationPosition, map)) {
-    return false;
-  }
-  if (getTwoDimensionalDistanceBetweenTwoPositions(start, destinationPosition) > 1) {
-    return false;
-  }
-  if (getAltitudeAtPosition(destinationPosition, map) > getAltitudeAtPosition(start, map) + 1) {
-    return false;
-  }
-  return true;
-}
-
-function arePositionsEqual(position1: Position, position2: Position): boolean {
-  return position1.x === position2.x && position1.y === position2.y;
-}
 
 function isPositionAlreadyVisited(currentPosition: HistoryTreeNode, positionToCheck: Position): boolean {
   if (arePositionsEqual(currentPosition.position, positionToCheck)) {
@@ -147,15 +75,16 @@ function createRoutesTree(map: Map, start: Position, end: Position): HistoryTree
     previousPosition: null,
     nextPositions: [],
   };
-  createRoutesTreeRecursive(map, end, rootTreeNode);
+  createRoutesTreeRecursive(map, end, rootTreeNode, rootTreeNode);
   return rootTreeNode;
 }
-let recursionCount = 0;
-function createRoutesTreeRecursive(map: Map, end: Position, currentTreeNode: HistoryTreeNode): void {
-  recursionCount++;
-  if (recursionCount === 1000000) {
-    console.log("got to 1000000");
-  }
+
+function createRoutesTreeRecursive(
+  map: Map,
+  end: Position,
+  rootTreeNode: HistoryTreeNode,
+  currentTreeNode: HistoryTreeNode
+): void {
   if (arePositionsEqual(currentTreeNode.position, end)) {
     return;
   }
@@ -165,11 +94,11 @@ function createRoutesTreeRecursive(map: Map, end: Position, currentTreeNode: His
     nextPositions: [],
   }));
   currentTreeNode.nextPositions.forEach((nextPosition) => {
-    createRoutesTreeRecursive(map, end, nextPosition);
+    createRoutesTreeRecursive(map, end, rootTreeNode, nextPosition);
   });
 }
 
-function findShortestRoute(map: Map, start: Position, end: Position): Move[] {
+function findShortestRoute(map: Map, start: Position, end: Position): Position[] {
   const routesTree = createRoutesTree(map, start, end);
 
   console.log(routesTree);
---
+++ days/day12/util.ts
@@ -0,0 +1,80 @@
+// a-z
+export type Altitude = number; // 0-25
+export type Map = Altitude[][];
+
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export enum Move {
+  UP = "up",
+  DOWN = "down",
+  LEFT = "left",
+  RIGHT = "right",
+}
+
+export function parseInputToMap(input: string): { map: Map; start: Position; end: Position } {
+  const start: Position = { x: -1, y: -1 };
+  const end: Position = { x: -1, y: -1 };
+
+  const map: Map = input.split("\n").map((line, yIndex) =>
+    line.split("").map((char, xIndex) => {
+      if (char === "S") {
+        start.x = xIndex;
+        start.y = yIndex;
+        return 0;
+      }
+      if (char === "E") {
+        end.x = xIndex;
+        end.y = yIndex;
+        return 25;
+      }
+      return char.charCodeAt(0) - 97;
+    })
+  );
+
+  if (start.x === -1 || start.y === -1 || end.x === -1 || end.y === -1) {
+    throw new Error("start or end not found");
+  }
+
+  return { map, start, end };
+}
+
+export function getAltitudeAtPosition(position: Position, map: Map): Altitude {
+  const res = map[position.y]?.[position.x];
+  if (res === undefined) {
+    throw new Error("position out of bounds");
+  }
+  return res;
+}
+
+export function isPositionInMap(position: Position, map: Map): boolean {
+  try {
+    getAltitudeAtPosition(position, map);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+export function getTwoDimensionalDistanceBetweenTwoPositions(position1: Position, position2: Position): number {
+  return Math.hypot(position2.x - position1.x, position2.y - position1.y);
+}
+
+export function isMovePossible(start: Position, destinationPosition: Position, map: Map): boolean {
+  if (!isPositionInMap(destinationPosition, map)) {
+    return false;
+  }
+  if (getTwoDimensionalDistanceBetweenTwoPositions(start, destinationPosition) > 1) {
+    return false;
+  }
+  if (getAltitudeAtPosition(destinationPosition, map) > getAltitudeAtPosition(start, map) + 1) {
+    return false;
+  }
+  return true;
+}
+
+export function arePositionsEqual(position1: Position, position2: Position): boolean {
+  return position1.x === position2.x && position1.y === position2.y;
+}
